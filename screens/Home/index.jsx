840fc59ac2d132a4d69121ea313014c71f80b857
Litzz03 <91480903+Litzz03@users.noreply.github.com>
2023-08-31 19:51:28 -0300

    criação do projeto

---
+++ screens/Home/index.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import styles from './styles'
+ 
+
+export function Home(){
+    
+    return(
+        <View>
+            <Text>testando</Text>
+        </View>
+    )
+}
